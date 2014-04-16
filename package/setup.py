11740853090dc5bd4e02ad36bce81b3c463b808a
Alexander <A.S.Pyatkin@gmail.com>
2014-04-16 10:45:20 +0400

    Added setup.py

--- package/setup.py
+++ package/setup.py
@@ -1,1 +1,17 @@
 from setuptools import setup, find_packages
+
+install_requires = [
+    "sqlalchemy"
+]
+
+setup(
+    name='assetoolz',
+    version='0.1',
+    description='Web assets preprocessor',
+    author='Alexander Pyatkin',
+    author_email='A.S.Pyatkin@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    data_files=[],
+    install_requires=install_requires
+)
