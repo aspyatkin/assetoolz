5342276031f8705ae9f3f540b822d1818e1c356e
Alexander <a.s.pyatkin@gmail.com>
2014-07-18 19:33:17 +0400

    Updated package dependencies

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from setuptools import setup, find_packages
 
-install_requires = [
-    'sqlalchemy'
-]
 
 setup(
     name='assetoolz',
-    version='0.0.0',
+    version='0.0.1',
     description='Web assets build system',
     author='Alexander Pyatkin',
     author_email='asp@thexyz.net',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     data_files=[],
-    install_requires=['setuptools', 'sqlalchemy', 'pyyaml'],
-    url='http://pypi.python.org/pypi/assetoolz',
+    install_requires=[
+        'setuptools',
+        'sqlalchemy>=0.9,<1.0',
+        'pyyaml>=3.11'
+    ],
+    platforms=['any'],
+    url='https://github.com/aspyatkin/assetoolz',
     license='MIT'
 )
